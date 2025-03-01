58dae9946912cb48318f2a2eaee136ef31ba7db6
Omni Adams <omni@digitaldarkness.com>
2025-03-01 16:06:26 +0000

    Fix TIC link on settings page
    The link on the settings page goes to "https://taxcloud.net/tic" which
    has SSL errors and the page shows "Error 403 - This web app is stopped."

--- includes/class-sst-settings.php
+++ includes/class-sst-settings.php
@@ -248,7 +248,7 @@ class SST_Settings {
 				),
 				'default'     => self::get_default_shipping_tic(),
 				'description' => __(
-					'Enter TIC code on <a href="https://taxcloud.net/tic" target="_blank">TaxCloud website</a> for details.',
+					'Enter TIC code on <a href="https://app.taxcloud.com/tic" target="_blank">TaxCloud website</a> for details.',
 					'simple-sales-tax'
 				),
 				'desc_tip'    => __(
