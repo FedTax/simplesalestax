35c7e48fe5bfc578615648a611e4e505fa92dbfc
Akhtarujjaman Shuvo <16398283+akshuvo@users.noreply.github.com>
2025-09-13 02:43:14 +0600

    Troubleshooting: Tax calculation issue (#371)
    * Bump version to 8.3.3
    * Tax calculation failures
    * Remove unhooked submenu code
    
    * Create a new log file every day for better debug
    
    * Cache verified address only
    
    * add logger for better debug
    
    * handle empty value
    
    * More Logging
    
    * fix:filter_hide_zero_taxes
    
    * Option: Show Zero Tax on Order Page
    
    * option: order_show_zero_tax implement
    
    * show_zero_tax: option reorder
    
    * Tests fixing

--- includes/class-sst-logger.php
+++ includes/class-sst-logger.php
@@ -38,6 +38,10 @@ class SST_Logger {
 	 */
 	public static function init() {
 		if ( 'yes' === SST_Settings::get( 'log_requests' ) ) {
+			/**
+			 * Create a new log file every day.
+			 */
+			self::$handle = self::$handle . '-' . date( 'Y-m-d' );
 			self::$logger = function_exists( 'wc_get_logger' ) ? wc_get_logger() : new WC_Logger();
 		}
 	}
@@ -59,11 +63,35 @@ class SST_Logger {
 	 *
 	 * @since 5.0
 	 */
-	public static function add( $message ) {
+	public static function add( $message, $context = array() ) {
 		if ( ! is_null( self::$logger ) ) {
-			self::$logger->add( self::$handle, $message );
+			self::$logger->notice( $message, array( 
+				'source' => self::$handle, 
+				'_context' => (array) $context 
+				) 
+			);
 		}
 	}
+
+
+	/**
+	 * Log a debug message.
+	 *
+	 * @param string $message Log message.
+	 * @param array  $context Log context.
+	 *
+	 * @since 8.3.3
+	 */
+	public static function debug( $message, $context = array() ) {
+		if ( ! is_null( self::$logger ) ) {
+			self::$logger->debug( $message, array( 
+				'source' => self::$handle, 
+				'_context' => (array) $context 
+				) 
+			);
+		}
+	}
+
 
 }
 
--- includes/class-sst-settings.php
+++ includes/class-sst-settings.php
@@ -202,7 +202,7 @@ class SST_Settings {
 				'description' => __( 'Control how taxes are displayed during checkout.', 'simple-sales-tax' ),
 			),
 			'show_zero_tax'               => array(
-				'title'       => __( 'Show Zero Tax?', 'simple-sales-tax' ),
+				'title'       => __( 'Show Zero Tax On Cart Page?', 'simple-sales-tax' ),
 				'type'        => 'select',
 				'options'     => array(
 					'true'  => __( 'Yes', 'simple-sales-tax' ),
@@ -214,6 +214,20 @@ class SST_Settings {
 					'simple-sales-tax'
 				),
 				'disabled'    => $disable_show_zero_tax,
+				'desc_tip'    => true,
+			),
+			'order_show_zero_tax'					=> array(
+				'title'       => __( 'Show Zero Tax on Order Page?', 'simple-sales-tax' ),
+				'type'        => 'select',
+				'options'     => array(
+					'false' => __( 'No', 'simple-sales-tax' ),
+					'true'  => __( 'Yes', 'simple-sales-tax' ),
+				),
+				'default'     => 'true',
+				'description' => __(
+					'When the sales tax due is zero, should the "Sales Tax" line be shown on the order page?',
+					'simple-sales-tax'
+				),
 				'desc_tip'    => true,
 			),
 			'advanced_settings'           => array(
@@ -263,6 +277,17 @@ class SST_Settings {
 				'default'     => 'yes',
 				'description' => __(
 					'When selected, TaxCloud for WooCommerce will log all requests sent to TaxCloud for debugging purposes.',
+					'simple-sales-tax'
+				),
+				'desc_tip'    => true,
+			),
+			'force_tax_lookup'						=> array(
+				'title'       => __( 'Force Tax Lookup', 'simple-sales-tax' ),
+				'type'        => 'checkbox',
+				'label'       => ' ',
+				'default'     => '',
+				'description' => __(
+					'When selected, TaxCloud for WooCommerce will force a tax lookup for each order regardless of cached results.',
 					'simple-sales-tax'
 				),
 				'desc_tip'    => true,
