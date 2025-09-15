d438b98a9230a42a8a8d2bf479709ed23667a2de
ffelizp-toptal <felix.feliz@toptal.com>
2025-09-15 15:51:25 -0400

    Add - New Setting to allow customer to disable or not the capture (#363)
    * Add - New Setting to allow customer to disable or not the capture
    
    * Log fix
    
    * settings merge
    
    * Order logging
    
    ---------
    
    Co-authored-by: Felix Feliz <felixfeliz@Felixs-MacBook-Pro.local>
    Co-authored-by: Akhtarujjaman Shuvo <aktarujjman@gmail.com>

--- includes/class-sst-order.php
+++ includes/class-sst-order.php
@@ -660,6 +660,20 @@ class SST_Order extends SST_Abstract_Cart {
 		// Logging
 		SST_Logger::order_log( __( 'Capturing order.', 'simplesalestax' ), $order->get_id() );
 
+		// Capture Order In TaxCloud is disabled.
+		if ( 'no' === SST_Settings::get( 'capture_orders_in_taxcloud' ) ) {
+			// Logging
+			SST_Logger::order_log( __( 'Capture Order In TaxCloud is disabled in plugin settings.', 'simplesalestax' ), $order->get_id() );
+			
+			SST_Logger::add(
+				sprintf(
+					'Capture Order In TaxCloud is disabled in plugin settings. Skipping Capture process for Order ID: %d',
+					$order->get_id()
+				)
+			);
+			return false;
+		}
+
 		// Let devs control whether the order is captured in TaxCloud.
 		if ( ! apply_filters( 'sst_should_capture_order', true, $order, $this ) ) {
 			// Note: This is considered a success for consistency with do_refund.
