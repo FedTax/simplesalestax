704849016f8d1322939810dce9bc7b28a6e380cc
Akhtarujjaman Shuvo <aktarujjman@gmail.com>
2025-12-02 05:58:41 +0600

    test push

--- simple-sales-tax.php
+++ simple-sales-tax.php
@@ -13,7 +13,7 @@
  * License:              GPLv2 or later
  *
  * Requires at least:    4.5.0
- * Tested up to:         6.8.0
+ * Tested up to:         6.8.3
  * WC requires at least: 6.9.0
  * WC tested up to:      10.2.1
  * Requires PHP:         7.4
