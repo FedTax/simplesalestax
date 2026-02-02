e1103b903381403aa83ff41ceb9724cfd1f2ce98
Liam Maier <lmaier@taxcloud>
2026-02-02 14:05:38 -0800

    Filter out exsize tax from fees

--- includes/abstracts/class-sst-abstract-cart.php
+++ includes/abstracts/class-sst-abstract-cart.php
@@ -228,6 +228,13 @@ abstract class SST_Abstract_Cart {
 
 		/* Add fees */
 		foreach ( $package['fees'] as $cart_id => $fee ) {
+			// Skip CO EXCISE TAX fee - it's a tax itself and shouldn't be sent to TaxCloud
+			$fee_id = is_object( $fee ) && isset( $fee->id ) ? $fee->id : ( is_string( $fee ) ? $fee : '' );
+			$fee_name = is_object( $fee ) && isset( $fee->name ) ? $fee->name : '';
+			if ( $fee_id === 'co-excise-tax' || $fee_id === 'CO EXCISE TAX' || $fee_name === 'CO EXCISE TAX' ) {
+				continue;
+			}
+
 			$cart_items[]     = new TaxCloud\CartItem(
 				count( $cart_items ),
 				$fee->id,
@@ -240,6 +247,7 @@ abstract class SST_Abstract_Cart {
 				'id'      => $fee->id,
 				'cart_id' => $cart_id,
 			);
+		}
 		}
 
 		/* Add shipping */
